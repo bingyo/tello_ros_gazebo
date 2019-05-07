9dd672c2650b0a7de4d0392aaff95975ea214e06
Hiroki BINGO <hirokibingo.ks@gmail.com>
2019-05-07 17:34:40 +0700

    Add rviz config

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 touch scripts/tellolib/__init__.py
 touch scripts/tellolib/Tello_Video/__init__.py
+chmod +x scripts/ros_tello.py
 cd scripts/tellolib/Tello_Video/install/Linux && source linux_install.sh
