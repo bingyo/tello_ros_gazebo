011654aedf0ba709ab4d8575c2f63cad859106f4
Hiroki BINGO <hirokibingo.ks@gmail.com>
2019-07-22 13:19:00 +0700

    Delte getKey function

--- tello_autonomous/scripts/circle.py
+++ tello_autonomous/scripts/circle.py
@@ -153,14 +153,6 @@ class ForestMotion:
 
         
 
-def getKey():
-    tty.setraw(sys.stdin.fileno())
-    select.select([sys.stdin], [], [], 0)
-    key = sys.stdin.read(1)
-    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
-    return key
-
-
 if __name__ == '__main__':
     settings = termios.tcgetattr(sys.stdin)
 
